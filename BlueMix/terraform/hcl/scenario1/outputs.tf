83bea5d4aa2938a77a108cc1d75cbd8a0b140230
jml <jm.lafont@fr.ibm.com>
2020-10-08 10:45:00 +0200

    change output (suppress public:)

--- BlueMix/terraform/hcl/scenario1/outputs.tf
+++ BlueMix/terraform/hcl/scenario1/outputs.tf
@@ -1,3 +1,3 @@
 output "vm_ip" {
-  value = "Public : ${ibm_compute_vm_instance.debian_small_virtual_guest.ipv4_address}"
+  value = "${ibm_compute_vm_instance.debian_small_virtual_guest.ipv4_address}"
 }
